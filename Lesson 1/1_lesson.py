c086a774d0808bad4b54a7dd5a2fe9d2ab1433bc
sejros <sairos@bk.ru>
2017-11-01 22:38:25 +0300

    linear regression

--- Lesson 1/1_lesson.py
+++ Lesson 1/1_lesson.py
@@ -6,16 +6,17 @@ data = sp.genfromtxt("web_traffic.tsv", delimiter="\t")
 #data.shape
 
 """ Разделяем данные на столбцы """
-x = data[:,0]
-y = data[:,1]
+x = data[:, 0]
+y = data[:, 1]
 
 """ Обнаруживаем и удаляем отсутствующие данные """
 #sp.sum(sp.isnan(y))
 #Out[37]: 8
-х = x[~sp.isnan(y)]
-у = y[~sp.isnan(y)]
 
+x = x[~sp.isnan(y)].copy()
+y = y[~sp.isnan(y)].copy()
 
+""" Строим наши данные на графике """
 import matplotlib.pyplot as plt
 
 plt.scatter(x, y, s=10)
@@ -29,6 +30,17 @@ plt.show()
 #bok.show(p)
 
 
-import seaborn as sns
+#import seaborn as sns
+#
+#sns.jointplot(x, y)
 
-sns.jointplot(x, y)
+
+""" Строим модель парной линейной регрессии """
+
+reg = sp.polyfit(x, y, deg=1)
+print(reg)
+fl = sp.poly1d(reg)
+fx = sp.linspace(0, x[-1], 1000)
+plt.scatter(x, y, s=10)
+plt.plot(fx, fl(fx), linewidth=4, color="red")
+plt.show()
