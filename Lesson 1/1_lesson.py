5415a4f538bde3c93b638361ad539bd4f24005f7
sejros <sairos@bk.ru>
2017-11-01 23:32:53 +0300

    added train-test split

--- Lesson 1/1_lesson.py
+++ Lesson 1/1_lesson.py
@@ -37,17 +37,18 @@ plt.show()
 """ Функция ошибки """
 
 def error(f, x, y):
-    return sp.sum((f(x) - y) ** 2)
+    from math import sqrt
+    return sqrt(sp.sum((f(x) - y) ** 2) / len(x))
 
 """ Строим модель парной линейной регрессии """
 
 reg = sp.polyfit(x, y, deg=1)
 f1 = sp.poly1d(reg)
 print(reg, error(f1, x, y))
-fx = sp.linspace(0, x[-1], 1000)
-plt.scatter(x, y, s=10)
-plt.plot(fx, f1(fx), linewidth=4, color="red")
-plt.show()
+#fx = sp.linspace(0, x[-1], 1000)
+#plt.scatter(x, y, s=10)
+#plt.plot(fx, f1(fx), linewidth=4, color="red")
+#plt.show()
 
 
 """ Модель парной квадратичной регрессии """
@@ -55,10 +56,10 @@ plt.show()
 reg = sp.polyfit(x, y, deg=2)
 f2 = sp.poly1d(reg)
 print(reg, error(f2, x, y))
-fx = sp.linspace(0, x[-1], 1000)
-plt.scatter(x, y, s=10)
-plt.plot(fx, f2(fx), linewidth=4, color="red")
-plt.show()
+#fx = sp.linspace(0, x[-1], 1000)
+#plt.scatter(x, y, s=10)
+#plt.plot(fx, f2(fx), linewidth=4, color="red")
+#plt.show()
 
 
 """ Регрессия 3 степени """
@@ -66,10 +67,10 @@ plt.show()
 reg = sp.polyfit(x, y, deg=3)
 f3 = sp.poly1d(reg)
 print(reg, error(f3, x, y))
-fx = sp.linspace(0, x[-1], 1000)
-plt.scatter(x, y, s=10)
-plt.plot(fx, f3(fx), linewidth=4, color="red")
-plt.show()
+#fx = sp.linspace(0, x[-1], 1000)
+#plt.scatter(x, y, s=10)
+#plt.plot(fx, f3(fx), linewidth=4, color="red")
+#plt.show()
 
 
 """ Регрессия 10 степени """
@@ -77,10 +78,10 @@ plt.show()
 reg = sp.polyfit(x, y, deg=10)
 f10 = sp.poly1d(reg)
 print(error(f10, x, y))
-fx = sp.linspace(0, x[-1], 1000)
-plt.scatter(x, y, s=10)
-plt.plot(fx, f10(fx), linewidth=4, color="red")
-plt.show()
+#fx = sp.linspace(0, x[-1], 1000)
+#plt.scatter(x, y, s=10)
+#plt.plot(fx, f10(fx), linewidth=4, color="red")
+#plt.show()
 
 
 """ Регрессия 100 степени """
@@ -88,14 +89,28 @@ plt.show()
 reg = sp.polyfit(x, y, deg=100)
 f100 = sp.poly1d(reg)
 print(error(f100, x, y))
-fx = sp.linspace(0, x[-1], 1000)
-plt.scatter(x, y, s=10)
-plt.plot(fx, f100(fx), linewidth=4, color="red")
-plt.show()
+#fx = sp.linspace(0, x[-1], 1000)
+#plt.scatter(x, y, s=10)
+#plt.plot(fx, f100(fx), linewidth=4, color="red")
+#plt.show()
 
 
 """ Строим регрессию используя вспомогательную функцию """
 
 from gists import plot_models
 
-plot_models(x, y, [f1], "test")
+plot_models(x, y, [f1, f2, f3, f10], "train")
+
+
+
+""" Разбиение выборки на тестовую и обучающую """
+
+from gists import train_test_split
+
+(train, test) = train_test_split(len(x), frac=0.8)
+reg = sp.polyfit(x[train], y[train], deg=2)
+f2_ = sp.poly1d(reg)
+print(error(f2_, x[train], y[train]))
+print(error(f2_, x[test], y[test]))
+
+plot_models(x, y, [f2_], "test")
