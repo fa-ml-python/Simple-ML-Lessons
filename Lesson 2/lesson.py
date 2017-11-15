523b9f9a7d9ab5fce66714d4b811c73b61f3b0dd
sejros <sairos@bk.ru>
2017-11-15 22:21:01 +0300

    Gradient descent into method

--- Lesson 2/lesson.py
+++ Lesson 2/lesson.py
@@ -16,11 +16,6 @@ gasp = pandas.read_csv("data/GAZP_101001_171001.txt")
 
 print(dj.shape, gasp.shape)
 
-dj = dj[:gasp.shape[0]]
-
-x = gasp['<CLOSE>']
-y = dj['<CLOSE>']
-
 res = pandas.merge(dj, gasp, on='<DATE>', suffixes=['_DJ', '_GASP'])
 x = res['<CLOSE>_DJ']
 y = res['<CLOSE>_GASP']
@@ -38,6 +33,24 @@ class hypothesis(object):
         return self.theta[0] + self.theta[1] * X
     def error(self, X, Y):
         return sum((self.apply(X) - Y)**2) / (2 * len(Y))
+    def gradient_descent(self, x, y, alpha=0.7):
+        i = 0
+        steps = []
+        errors = []
+        m = len(y)
+        while(i < 150):
+            y_ = hyp.apply(x)
+            dJ0 = sum(y_ - y) / m
+            dJ1 = sum((y_ - y)*x) / m
+            theta0 = self.theta[0] - alpha * dJ0
+            theta1 = self.theta[1] - alpha * dJ1
+            self.theta = sp.array([theta0, theta1])
+            
+            steps.append(i)
+            errors.append(hyp.error(x, y))
+            
+            i += 1
+        return (steps, errors)
 
 
 hyp = hypothesis()
@@ -48,33 +61,32 @@ plt.plot(x, y_, color="red")
 J = hyp.error(x, y)
 print(J)
 
-i = 0
-steps = []
-errors = []
-while(i < 150):
-    y_ = hyp.apply(x)
-    dJ0 = sum(y_ - y) / m
-    dJ1 = sum((y_ - y)*x) / m
-#    print(dJ0, dJ1)
-    
-    alpha = 0.7
-    theta0 = hyp.theta[0] - alpha * dJ0
-    theta1 = hyp.theta[1] - alpha * dJ1
-    hyp.theta = sp.array([theta0, theta1])
-#    print(hyp.theta)
-    
-    J = hyp.error(x, y)
-#    print(J)
-    
-    steps.append(i)
-    errors.append(J)
-    
-    i += 1
+#i = 0
+#steps = []
+#errors = []
+#while(i < 150):
+#    y_ = hyp.apply(x)
+#    dJ0 = sum(y_ - y) / m
+#    dJ1 = sum((y_ - y)*x) / m
+#    
+#    alpha = 0.7
+#    theta0 = hyp.theta[0] - alpha * dJ0
+#    theta1 = hyp.theta[1] - alpha * dJ1
+#    hyp.theta = sp.array([theta0, theta1])
+#    
+#    J = hyp.error(x, y)
+#    
+#    steps.append(i)
+#    errors.append(J)
+#    
+#    i += 1
 
+(steps, errors) = hyp.gradient_descent(x, y)
+y_ = hyp.apply(x)
 plt.plot(x, y_, color="green")
 plt.show()
 
-print(errors[-1])
+print(errors[-1] - errors[-2])
 
 plt.plot(steps, errors)
 plt.show()
