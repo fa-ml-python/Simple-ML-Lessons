60421c82f4a4b921358231a129ec1fee8a00efc3
sejros <sairos@bk.ru>
2017-12-17 21:28:57 +0300

    Tru Nadam with ReLU

--- Lesson 5. Keras/1. Diabetes.py
+++ Lesson 5. Keras/1. Diabetes.py
@@ -13,7 +13,7 @@ Y = dataset[:,8]
 
 # create first model
 model = Sequential()
-model.add(Dense(8, input_dim=8, activation='sigmoid'))
+model.add(Dense(8, input_dim=8, activation='relu'))
 #model.add(Dense(8, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
 # Compile model
@@ -23,4 +23,4 @@ model.fit(X, Y, epochs=150, batch_size=10)
 # evaluate the model
 scores = model.evaluate(X, Y)
 print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
-''' 72.66% '''
+''' 78.26% '''
