e006cd5859fb50d2d687e133c4ae53572a14677b
sejros <sairos@bk.ru>
2017-12-17 22:33:40 +0300

    Simpler MNIST net

--- Lesson 5. Keras/2. MNIST.py
+++ Lesson 5. Keras/2. MNIST.py
@@ -34,17 +34,17 @@ y_test = to_categorical(y_test, 10)
  
 # 7. Define model architecture
 model = Sequential()
-model.add(Dense(512, activation='relu', input_shape=(784,)))
-model.add(Dropout(0.2))
-model.add(Dense(512, activation='relu'))
-model.add(Dropout(0.2))
+model.add(Dense(100, activation='relu', input_shape=(784,)))
+#model.add(Dropout(0.2))
+#model.add(Dense(512, activation='relu'))
+#model.add(Dropout(0.2))
 model.add(Dense(num_classes, activation='softmax'))
 
 model.summary()
  
 # 8. Compile model
 model.compile(loss='categorical_crossentropy',
-              optimizer=RMSprop(),
+              optimizer='RMSprop',
               metrics=['accuracy'])
  
 # 9. Fit model on training data
